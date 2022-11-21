4281ee5fcc3582555e69dd120fc471379e046cf7
Daniel Melo <daniel.melo42@outlook.com>
2022-11-21 09:13:22 -0700

    build

---
+++ build/js/scripts.js
@@ -0,0 +1,5 @@
+/*!
+* Start Bootstrap - Resume v7.0.5 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/window.addEventListener("DOMContentLoaded",o=>{document.body.querySelector("#sideNav")&&new bootstrap.ScrollSpy(document.body,{target:"#sideNav",offset:74});const e=document.body.querySelector(".navbar-toggler");[].slice.call(document.querySelectorAll("#navbarResponsive .nav-link")).map(function(n){n.addEventListener("click",()=>{window.getComputedStyle(e).display!=="none"&&e.click()})})});
